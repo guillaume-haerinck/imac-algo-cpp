d45a3063ca858b83ca6c8fc1f43db318423920cb
guillaume-haerinck <guillaume.haerinck@outlook.com>
2018-12-04 12:30:20 +0100

    pop list

--- src/cours-8-9/ex1.c
+++ src/cours-8-9/ex1.c
@@ -28,12 +28,32 @@ void push(list* head, int value) {
     current->next->next = NULL;
 }
 
+void pop(list* head) {
+    /* if there is only one item */
+    if (head->next == NULL) {
+        free(head);
+        return;
+    }
+
+    /* find the item before the last */
+    list* current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+
+    /* remove the last value */
+    current->next = NULL;
+    free(current->next);
+}
+
 int main() {
     list* head = NULL;
     head = malloc(sizeof(list));
     head->value = 0x808080;
+
     push(head, 0x000000); // black
     push(head, 0xFF0000); // red
     push(head, 0xC0C0C0); // metal
+    pop(head);
     printList(head);
 }
