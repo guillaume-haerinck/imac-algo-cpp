6f57c2312d4d55587bb4626c2965146fbff1071d
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-02-26 11:55:09 +0100

    binary search

--- src/semestre-2/tp-3/exo1.cpp
+++ src/semestre-2/tp-3/exo1.cpp
@@ -7,7 +7,27 @@ using std::size_t;
 
 int binarySearch(Array& array, int toSearch)
 {
+    int l = 0;
+    int r = array.size() - 1;
+    while (l <= r) {
+            int m = l + (r - l) / 2;
 
+            // Check if x is present at mid
+            if (array.get(m) == toSearch)
+                return m;
+
+            // If x greater, ignore left half
+            if (array.get(m) < toSearch)
+                l = m + 1;
+
+            // If x is smaller, ignore right half
+            else
+                r = m - 1;
+        }
+
+        // if we reach here, then element was
+        // not present
+        return -1;
 }
 
 int main(int argc, char *argv[])
