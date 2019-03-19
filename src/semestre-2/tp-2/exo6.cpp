024d54dc596ce28e5ee03a9a4354eb6524b40de7
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-03-19 11:08:01 +0100

    recursive quicksort

--- src/semestre-2/tp-2/exo6.cpp
+++ src/semestre-2/tp-2/exo6.cpp
@@ -2,8 +2,29 @@
 #include <QApplication>
 #include <time.h>
 
-void quickSort(Array& toSort) {
-    // TODO
+int partition(Array& toSort, int begin, int end){
+    int pivot = toSort[end];
+    int i = begin;
+    for(int j=begin;j<end; j++){
+        if(toSort[j]<pivot){
+            toSort.swap(i, j);
+            i=i+1;
+        }
+    }
+    toSort.swap(i, end);
+    return i;
+}
+
+void quickSortAux(Array& toSort , int begin , int end){
+    if( begin < end ){
+        int p = partition(toSort, begin, end);
+        quickSortAux(toSort, begin, p-1);
+        quickSortAux(toSort, p+1, end);
+    }
+}
+
+void quickSort(Array& toSort){
+    quickSortAux(toSort, 0, toSort.size()-1);
 }
 
 int main(int argc, char *argv[]) {
