a84c57262ebcd61167d41015a1e24f55cf0f4d4d
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-10-17 11:52:40 +0200

    strassen almost done

--- src/semestre-3/maths-2/ex5/main.cpp
+++ src/semestre-3/maths-2/ex5/main.cpp
@@ -6,8 +6,32 @@ Eigen::MatrixXd StrassenMultiply(const Eigen::MatrixXd& A, const Eigen::MatrixXd
         return A * B;
     }
 
-    // TODO recursive algorithm
-    
+    unsigned int rows = A.rows() / 2;
+    unsigned int cols = A.cols() / 2;
+
+    auto a = A.topLeftCorner(rows, cols);
+    auto b = A.topRightCorner(rows, cols);
+    auto c = A.bottomLeftCorner(rows, cols);
+    auto d = A.bottomRightCorner(rows, cols);
+    auto e = B.topLeftCorner(rows, cols);
+    auto f = B.topRightCorner(rows, cols);
+    auto g = B.bottomLeftCorner(rows, cols);
+    auto h = B.bottomRightCorner(rows, cols);
+
+    Eigen::MatrixXd P1 = StrassenMultiply(a, f - h);
+    Eigen::MatrixXd P2 = StrassenMultiply(a + b, h);
+    Eigen::MatrixXd P3 = StrassenMultiply(c + d, e);
+    Eigen::MatrixXd P4 = StrassenMultiply(d, g - e);
+    Eigen::MatrixXd P5 = StrassenMultiply(a + d, e + h);
+    Eigen::MatrixXd P6 = StrassenMultiply(b - d, g + h);
+    Eigen::MatrixXd P7 = StrassenMultiply(a - c, e + f);
+
+    Eigen::MatrixXd r = P5 + P4 - P2 + P6;
+    Eigen::MatrixXd s = P1 + P2;
+    Eigen::MatrixXd t = P3 + P4;
+    Eigen::MatrixXd u = P1 + P5 - P3 - P7;
+
+    // TODO return matrix rstu
 }
 
 int main(int argc, char const *argv[]) {
