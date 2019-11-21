447b5219f08e7d2ba361ecafdbcfbf1ede20495d
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-11-21 11:36:19 +0100

    done tp math 3

--- src/semestre-3/maths-3/main.cpp
+++ src/semestre-3/maths-3/main.cpp
@@ -13,27 +13,26 @@ Eigen::VectorXd GS(const Eigen::MatrixXd& A, const Eigen::VectorXd& b, const uns
     auto x = b;
     for (size_t iter = 0; iter < nbIter; ++iter) {
         for (size_t i = 0; i < A.rows(); i++) {
-            double sum = 0.0f;
-            for (size_t j = 0; i < A.cols(); j++) {
+            double sum = 0.0;
+            for (size_t j = 0; j < A.cols(); j++) {
                 if (j != i) {
                     sum += A(i, j) * x(j);
                 }
                 x(i) = (b(i) - sum) / A(i, i);
             }
         }
+
+        std::cout << "iter : " << static_cast<unsigned int>(iter) << " : " << (A * x - b).norm() << std::endl;
     }
     return x;
 }
 
 int main(int argc, char const *argv[]) {
-    Eigen::MatrixXd A = Eigen::MatrixXd::Random(60, 60);
-    Eigen::VectorXd b = Eigen::VectorXd::Random(60);
+    Eigen::MatrixXd A = Eigen::MatrixXd::Random(5, 5);
+    Eigen::VectorXd b = Eigen::VectorXd::Random(5);
 
     buildSystem(A, b, A.size());
-    Eigen::VectorXd x = GS(A, b, 40);
-
-    auto erreur = (A * x - b).norm();
-    std::cout << erreur << std::endl;
+    Eigen::VectorXd x = GS(A, b, 16);
 
     return 0;
 }
